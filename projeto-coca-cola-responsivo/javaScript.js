cf1577c30e44f30b85557b315cda189fdbcac24d
Leonardo <leonardopereirasillva2020@gmail.com>
2022-12-13 09:30:48 -0300

    coca

---
+++ projeto-coca-cola-responsivo/javaScript.js
@@ -0,0 +1,16 @@
+const header = document.getElementById('cabeçalho');
+const logo = document.getElementById('logo-coca');
+const main = document.getElementById('conteudo-principal');
+const body = document.getElementById('body');
+
+function btnBurguer() {
+    header.style.display = "none";
+    body.style.backgroundColor = "white";
+    
+}
+
+function btnFechar() {
+    header.style.display = "block";
+    main.style.display = "none";
+    body.style.backgroundColor = "red";
+}
